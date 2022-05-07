2c61aee19845f6663b4bb64f3550303775f16398
Mark <72723296+markl12s@users.noreply.github.com>
2022-05-07 15:23:11 -0700

    slight refactoring

--- sortingAlgViz.py
+++ sortingAlgViz.py
@@ -19,12 +19,12 @@ class array_data:
     unsortedArray = []
     currentSorts = []
 
-    def create_array(self, size):
+    def create_array(size):
         for i in range(size):
             array_data.unsortedArray.append(random.randint(1, 100))
             array_data.currentSorts.append('black')
 
-    def reset_colors(self, array):
+    def reset_colors(array):
         for i in range(len(array)):
             array[i] = 'black'
 
@@ -38,20 +38,20 @@ visualization
 class visualization:
     pen = turtle.Turtle()
 
-    def update_screen(self, timePerFrame, array):
+    def update_screen(timePerFrame, array):
         window.tracer(0, 60)
-        visualization.draw_array('draw', array)
+        visualization.draw_array(array)
         window.update()
         time.sleep(timePerFrame)
         visualization.pen.clear()
 
-    def draw_array(self, heights):
+    def draw_array(heights):
         botLeft = 1
         for i in range(len(array_data.currentSorts)):
-            visualization.draw_block('block', [botLeft, 0], [botLeft + 2, heights[i]], array_data.currentSorts[i])
+            visualization.draw_block([botLeft, 0], [botLeft + 2, heights[i]], array_data.currentSorts[i])
             botLeft += 2
 
-    def draw_block(self, botLeft, topRight, color):
+    def draw_block(botLeft, topRight, color):
         visualization.pen.hideturtle()
         visualization.pen.speed(0)
         visualization.pen.color(color)
@@ -72,7 +72,7 @@ bubble sort
 """
 
 class bubble_sort:
-    def runSort(self, integerArray, colorArray, isSorted):
+    def runSort(integerArray, colorArray, isSorted, timePerFrame):
         sortsLeft = len(integerArray) - 1
 
         while not isSorted:
@@ -83,12 +83,11 @@ class bubble_sort:
 
                 if integerArray[i] > integerArray[i + 1]:
                     toSwitch = True
-                    colorArray[i + 1] = 'red'
                     isSorted = False
+                    colorArray[i + 1] = 'red'
 
-                visualization.update_screen('screen update', 0.05, array_data.unsortedArray)
-
-                array_data.reset_colors('reset', colorArray)
+                visualization.update_screen(timePerFrame, array_data.unsortedArray)
+                array_data.reset_colors(colorArray)
 
                 if toSwitch:
                     integerArray[i], integerArray[i + 1] = integerArray[i + 1], integerArray[i]
@@ -108,9 +107,9 @@ execution
 window = turtle.Screen()
 window.setworldcoordinates(0, 0, 100, 100)
 window.tracer(0)
-array_data.create_array('array', 50)
+array_data.create_array(50)
 time.sleep(3)
 
 # run sort
-bubble_sort.runSort('bubble', array_data.unsortedArray, array_data.currentSorts, False)
+bubble_sort.runSort(array_data.unsortedArray, array_data.currentSorts, False, 0.05)
 time.sleep(2)
